ddca9743a19e3d92c6914d6c39c35bc32f83b495
juliocrodrigues <juliocesarccr29@gmail.com>
2024-09-30 09:57:32 -0300

    Introdução de links das paginas e base das paginas

--- cp-4-front-end/src/components/Menu/Menu.tsx
+++ cp-4-front-end/src/components/Menu/Menu.tsx
@@ -4,7 +4,10 @@ export default function Menu() {
     return (
         <nav>
             <ul>
-                <li><Link href="/">Home</Link></li>
+                <li><Link href="">Catástrofes Planetárias no Passado Recente</Link></li>
+                <li><Link href="/">Aproximação de Vênus e sua Relação com a História da Terra</Link></li>
+                <li><Link href="/">Pertubações Gravitacionais entre Marte e a Terra</Link></li>
+                <li><Link href="/">Cálculo de Forças Gravitacionais</Link></li>
             </ul>
         </nav>
     )
