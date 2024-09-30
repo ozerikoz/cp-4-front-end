a73f5c6a2a8f734b587d7b6749e709e645c44974
ozerikoz <erik.paschoalatto1@gmail.com>
2024-09-30 09:09:57 -0300

    add componente Rodape

---
+++ cp-4-front-end/src/components/Rodape/Rodape.tsx
@@ -0,0 +1,16 @@
+
+export default function Rodape() {
+    return (
+        <footer>
+            <h2>Rodapé</h2>
+            <p>
+                Desenvolvido por
+                <span>
+                    Erik Paschoalatto dos Santos RM554854
+                </span>
+                <span>&</span>
+                <span>Julio Cesar Conceição Rodrigues RM557298</span>
+            </p>
+        </footer>
+    )
+}
