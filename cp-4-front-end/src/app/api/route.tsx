c1e9e09c484df769405410a59e2fadff5ce86f0f
ozerikoz <erik.paschoalatto1@gmail.com>
2024-09-30 09:26:17 -0300

    add endpoint para pegar as imagens do dia em route.tsx

---
+++ cp-4-front-end/src/app/api/route.tsx
@@ -0,0 +1,13 @@
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+    try {
+        const response = await fetch("https://api.nasa.gov/planetary/apod?count=3&apy_key=InxMVgAdl2ALeiKCAHnSgMmlRXxN3XNDG6Egj1rD");
+        const apod = await response.json()
+
+        return NextResponse.json(apod);
+    } catch (error) {
+        console.error("Erro ao chamar API", error);
+        return NextResponse.json({ message: "Erro no GET das imagens do dia" }, { status: 500 });
+    }
+}
