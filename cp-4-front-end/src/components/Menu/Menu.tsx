8d2f953da4291dde26def56c78b98c97011c7a3e
ozerikoz <erik.paschoalatto1@gmail.com>
2024-09-30 22:29:58 -0300

    add estilizacao no componente Menu

--- cp-4-front-end/src/components/Menu/Menu.tsx
+++ cp-4-front-end/src/components/Menu/Menu.tsx
@@ -1,14 +1,26 @@
 import Link from "next/link";
 
+import "../../app/globals.css";
+
 export default function Menu() {
-    return (
-        <nav>
-            <ul>
-                <li><Link href="">Catástrofes Planetárias no Passado Recente</Link></li>
-                <li><Link href="/">Aproximação de Vênus e sua Relação com a História da Terra</Link></li>
-                <li><Link href="/">Pertubações Gravitacionais entre Marte e a Terra</Link></li>
-                <li><Link href="/">Cálculo de Forças Gravitacionais</Link></li>
-            </ul>
-        </nav>
-    )
+  return (
+    <nav className="flex flex-row items-center gap-4 text-slate-50 py-4 font-bold">
+      <ul className="flex flex-row justify-between w-full px-16">
+        <li>
+          <Link href="">Catástrofes Planetárias no Passado Recente</Link>
+        </li>
+        <li>
+          <Link href="/">
+            Aproximação de Vênus e sua Relação com a História da Terra
+          </Link>
+        </li>
+        <li>
+          <Link href="/">Pertubações Gravitacionais entre Marte e a Terra</Link>
+        </li>
+        <li>
+          <Link href="/">Cálculo de Forças Gravitacionais</Link>
+        </li>
+      </ul>
+    </nav>
+  );
 }
