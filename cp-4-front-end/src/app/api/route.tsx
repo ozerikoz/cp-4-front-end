8abcad6e3650347e9758aaa65819abdc0afe7a87
ozerikoz <erik.paschoalatto1@gmail.com>
2024-09-30 23:30:26 -0300

    ajuste na url de route.tsx

--- cp-4-front-end/src/app/api/route.tsx
+++ cp-4-front-end/src/app/api/route.tsx
@@ -1,13 +1,19 @@
 import { NextResponse } from "next/server";
 
-export async function GET(request: Request) {
-    try {
-        const response = await fetch("https://api.nasa.gov/planetary/apod?count=3&apy_key=InxMVgAdl2ALeiKCAHnSgMmlRXxN3XNDG6Egj1rD");
-        const apod = await response.json()
+export async function GET() {
+  try {
+    const response = await fetch(
+      "https://api.nasa.gov/planetary/apod?count=3&api_key=InxMVgAdl2ALeiKCAHnSgMmlRXxN3XNDG6Egj1rD"
+    );
 
-        return NextResponse.json(apod);
-    } catch (error) {
-        console.error("Erro ao chamar API", error);
-        return NextResponse.json({ message: "Erro no GET das imagens do dia" }, { status: 500 });
-    }
+    const apod = await response.json();
+
+    return NextResponse.json(apod);
+  } catch (error) {
+    console.error("Erro ao chamar API", error);
+    return NextResponse.json(
+      { message: "Erro no GET das imagens do dia" },
+      { status: 500 }
+    );
+  }
 }
